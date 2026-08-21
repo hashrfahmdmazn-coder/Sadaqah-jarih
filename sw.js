45ff36b59b3f8c1d007180067f8c20ff43eed86e
hashrfahmdmazn-coder <hashrfahmdmazn@gmail.com>
2026-08-22 01:28:07 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
